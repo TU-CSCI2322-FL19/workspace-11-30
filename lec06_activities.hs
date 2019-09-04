265c725a04043b6d4889b522c2c11367d9954b1f
Seth Fogarty <sfogarty@trinity.edu>
2019-09-04 12:14:50 -0500

    oops

--- lec06_activities.hs
+++ lec06_activities.hs
@@ -1,9 +1,10 @@
--- Write a function sumEvensLC that takes a list and returns the sum of all even numbers, using a
--- list comprehension and the build-in sum funciton.
-sumEvensLc = undefined
+-- Write a function prodEvensLC that takes a list and returns the product of all even 
+-- numbers, using a list comprehension and the build-in product funciton.
+prodEvensLc = undefined
 
--- Write a recursive function sumEvensRec that takes a list and returns the sum of all even numbers.
-sumEvensRec = undefined
+-- Write a recursive function prodEvensRec that takes a list and returns the product of all even
+-- numbers.
+prodEvensRec = undefined
 
 -- Write a recursive function myMaximum that takes a list and returns the largest element.
 myMaximum = undefined
