86ae9f6d91c3523600d8ce2a89fd31e2e2fb400f
Seth Fogarty <sfogarty@trinity.edu>
2019-09-06 12:06:33 -0500

    push

--- lec06_activities.hs
+++ lec06_activities.hs
@@ -1,13 +1,12 @@
 
 --prodEvensLc :: [a] -> a
-prodEvensLc lst = product [ x | x <- lst, even x]
+prodEvensLc lst = undefined
 
 -- Write a recursive function prodEvensRec that takes a list and returns the product of all even
 -- numbers.
 --prodEvensRec lst 
 --prodEvensRec [x] = if even x then x * x else 0
-prodEvensRec [] = 0
-prodEvensRec(x:xs) = (if even x then x else 0) * prodEvensRec x
+prodEvensRec = undefined
 
 
 
