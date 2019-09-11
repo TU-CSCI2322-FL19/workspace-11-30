be44b203fc3cc4b3dbcecf5774727728fb632154
Seth Fogarty <sfogarty@trinity.edu>
2019-09-11 12:20:49 -0500

    push

--- lec08.hs
+++ lec08.hs
@@ -23,4 +23,39 @@ frev lst =
 
 
 gap :: [Int] -> Int
-gap lst = maximum lst - minimum lst
+gap [] = error "Gap of an empty list!"
+gap lst = 
+  let aux large small [] = large - small
+      aux large small (x:xs) -- = aux (max x large) (min x small) xs
+            | x < small = aux large x xs
+            | x > large = aux x small xs
+            | otherwise = aux large small xs
+  in aux (head lst) (head lst) lst
+
+
+range :: [Int] -> (Int, Int)
+range [] = error "AAAAH"
+range [x] = (x,x)
+range (x:xs) = 
+      let (large, small) = range xs
+      in (max x large, min x small)
+
+gap2 :: [Int] -> Int
+gap2 lst = let (l,s) = range lst  in l-s
+
+hoursToSchedule :: [(Int, Int)] -> Int
+hoursToSchedule tasks = 
+    let sumTimes :: [(Int, Int)] -> (Int, Int)
+        sumTimes [] = (0,0)
+        sumTimes ((h,m):ts) =
+            let (hoursTs, minTs) = sumTimes ts
+                totalHours = hoursTs + h
+                totalMins = minTs + m
+            in (totalHours + totalMins `div` 60, totalMins `mod` 60)
+        (hours,mins) = sumTimes tasks
+    in hours + if mins > 0 then 1 else 0
+{-
+hoursToSchedule ((h,m):ts) =
+    let hoursForTs = hoursToSchedule ts
+    in hoursForTs + h + if m > 0 then 1 else 0
+    -}
