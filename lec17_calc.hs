a2cad574fea266d139bd4d95df60c2a634af0a5b
Seth Fogarty <sfogarty@trinity.edu>
2019-10-21 11:42:02 -0500

    push

--- lec17_calc.hs
+++ lec17_calc.hs
@@ -1,6 +1,4 @@
 import Data.Char
-input = "+ 7 / 232 * 3 4"
-inputWords = words input
 
 -- data Token = PlusT | MinusT | MultT | DivT | NumT Integer deriving (Eq, Show)
 
@@ -29,11 +27,14 @@ lexer str = if all isDigit str
             then Just $ NumT $ read str
             else Nothing
 
-(Just tokens) = sequence $ map lexer inputWords
 --do not use this refutable pattern or fromJust outside of a class activity. Ever.
 
 data AST = Leaf Integer | Node Op AST AST deriving (Show, Eq)
 
+input = "+ 7 / 232 * 3 4"
+input2 = "+ * 2 3 * 4 2"
+inputWords = words input
+(Just tokens) = sequence $ map lexer inputWords
 goalAST = Node Plus (Leaf 7) (Node Div (Leaf 232) (Node Mult (Leaf 3) (Leaf 4)))
 
 --Step 2
@@ -41,15 +42,32 @@ dangerParse :: [Token] -> AST
 dangerParse = undefined
 
 --test on input and input2
-input2 = "+ * 2 3 * 4 2"
 
 
 --Step 1
 eval :: AST -> Integer
-eval = undefined
+eval (Leaf x) = x
+eval (Node Plus lft rgt) = eval lft + eval rgt
+eval (Node Mult lft rgt) = eval lft * eval rgt
+eval (Node Minus lft rgt) = eval lft - eval rgt
+eval (Node Div lft rgt) = eval lft `div` eval rgt
+
+eval2 :: AST -> Integer
+eval2 (Leaf x) = x
+eval2 (Node op lft rgt) = (evalOp op) (eval2 lft) (eval2 rgt)
+
+evalOp :: Op -> Integer -> Integer -> Integer
+evalOp Plus = (+)
+evalOp Mult = (*)
+evalOp Div = div 
+evalOp Minus = (-)
 
 --step 0
 isOp :: Token -> Bool
-isOp = undefined
+isOp (NumT x) = False
+isOp (OperT op) = True
+
 countOps :: AST -> Integer
-countOps = undefined
+countOps (Leaf x) = 0
+countOps (Node op lft rgt) = 1 + countOps lft + countOps rgt
+
