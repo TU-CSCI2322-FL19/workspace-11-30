321113dd946a7485b2cd60f04635f790026fe131
Seth Fogarty <sfogarty@trinity.edu>
2019-11-20 12:19:17 -0600

    push

--- Fortunes.hs
+++ Fortunes.hs
@@ -1,51 +1,78 @@
 import Data.Char
---Problems 0-2 will be done as a class.
---  0) Stop reading in the file every time.
---  1) Make an action to prompt the user and get a response.
---
---Core Exercise: Problems 2-4
---  2) Ask their name once. Remember it and print it out each time. Ask for a number for the
---  fortune.
---  3) Make an action getBool :: IO Bool to get a Yes/No response. 
---  Accept yes/y no/n in any captilization. If they input anything else, ask again.
---  4) Stop asking for a number: just give them the next fortune in the list.
+import System.Environment
+import System.Console.GetOpt
+import Text.Read
 
---Extra Fun Problems: Problems 5-7
---  5) Feature creep! Ask the user what they want, and support the following operations. 
---  Get each one working before you move on to the next.
---  Hint: Make an "Request" data-type and a parseRequest:: String -> Request function. 
---   a) "Give me a fortune" (or any string with fortune in it)
---      Print out the next fortune
---   b) "Remember _______" 
---      Remember whatever comes after the command, and print it out on every prompt
---   c) "Remind me" 
---      Only print out the string when told to remind them
---  6) Make parseRequest more robust: return a Maybe Request. 
---  7) Add an action "What is _____" that evaluates a prefix mathematical expression and prints it
---     out. You'll need to load Calc.hs.
+data Flag = Help | Name String | All | Count String deriving (Eq, Show)
+
+options :: [OptDescr Flag]
+options = [ Option ['h'] ["help"] (NoArg Help) "Print usage information and exit."
+          , Option ['n'] ["name"] (ReqArg Name "<name>") "Define the user's name."
+          , Option [] ["all","everything"] (NoArg All) "Print every fortune. On your own head be it."
+          , Option ['k','c'] ["count"] (ReqArg Count "<k>") "Print out <k> fortunes."
+          ]
 
 main :: IO ()
 main = do
-    allFortunes <- readFile "fortunes.txt"
-    let fortunes = lines allFortunes
-    giveFortune fortunes
+    args <- getArgs
+    let (flags, inputs, errors) = getOpt Permute options args
+    putStrLn $ show (flags, inputs, errors)
+    if Help `elem` flags || not (null errors) || length inputs > 1
+    then do mapM putStr errors
+            putStrLn $ usageInfo "Usage: fortunes [options] [file]" options
+    else do let filename = if null inputs then "fortunes.txt" else  head inputs
+            allFortunes <- readFile filename
+            let fortunes = lines allFortunes
+            if All `elem` flags
+            then giveAllFortunes fortunes
+            else do name <- getName flags
+                    case getCount flags of 
+                      Nothing -> giveFortune fortunes name
+                      Just c -> giveCountFortunes fortunes name c
 
-giveFortune :: [String] -> IO ()
-giveFortune fortunes = do
-    name <- prompt "Please enter your name: "
-    putStrLn $ "Hello " ++ name ++ ", here is your fortune."
-    putStrLn $ "\t" ++ (getFortune name fortunes)
-    response <- prompt "Would you like another fortune? "
-    if map toLower response `elem` ["y","yes","sure","please","ok"]
-    then main
-    else return ()
+getName :: [Flag] -> IO String
+getName (Name s:_) = return s
+getName (_:flags) = getName flags
+getName [] =  prompt "Please enter your name: "
+
+getCount :: [Flag] -> Maybe Int
+getCount (Count s:_) = readMaybe s
+getCount (_:flags) = getCount flags
+getCount [] =  Nothing
+
+giveAllFortunes :: [String] -> IO ()
+giveAllFortunes fortunes =
+  do sequence $ map putStrLn fortunes
+     return ()
+
+giveFortune :: [String] -> String -> IO ()
+giveFortune fortunes name = aux 0
+  where aux i = do putStrLn $ "Hello " ++ name ++ ", here is your fortune."
+                   putStrLn $ "\t" ++ (getFortune name fortunes i)
+                   response <- prompt "Would you like another fortune? "
+                   if map toLower response `elem` ["y","yes","sure","please","ok"]
+                   then aux (i+1)
+                   else return ()
+
+giveCountFortunes :: [String] -> String -> Int -> IO ()
+giveCountFortunes fortunes name count = 
+      do putStrLn $ "Hello " ++ name ++ ", here is your fortune."
+         let cFortunes = getCountFortunes name fortunes count
+         sequence $ map (\s -> putStrLn $ "\t"++s) cFortunes
+         return () 
 
 prompt :: String -> IO String
 prompt message = 
   do putStr message
      getLine
 
-getFortune :: String -> [String] -> String
-getFortune name list = 
+getFortune :: String -> [String] -> Int -> String
+getFortune name list i= 
     let index = sum [ord c | c <- name] `mod` length list
-    in list!!index
+    in list!!(index+i)
+
+getCountFortunes :: String -> [String] -> Int -> [String]
+getCountFortunes name list count = 
+    let index = sum [ord c | c <- name] `mod` length list
+    in take count $ drop (index-1) list
+    
